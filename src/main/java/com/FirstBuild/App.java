b19e1658f425a41ade4a5bc682fcff1c531c3e6e
git <root@ip-172-31-44-246.ap-south-1.compute.internal>
2026-04-30 14:36:53 +0000

    Updates

--- src/main/java/com/FirstBuild/App.java
+++ src/main/java/com/FirstBuild/App.java
@@ -2,7 +2,7 @@ package com.FirstBuild;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Java Jenkins Pipeline!");
+        System.out.println("Hello Java");
     }
 
     public static String message() {
