98717935e4b603d5cc8dee72e54b75018bf38516
d4716573-blip <d4716573@gmail.com>
2026-05-05 18:30:11 +0530

    Refactor App.java for Spring Boot application
    Updated package name and modified main method to run Spring Boot application. Added a REST endpoint for a greeting message.

--- src/main/java/com/FirstBuild/App.java
+++ src/main/java/com/FirstBuild/App.java
@@ -1,11 +1,20 @@
-package com.FirstBuild;
+package com.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        SpringApplication.run(App.class, args);
     }
 
-    public static String message() {
-        return "Jenkins Maven Build Success";
+    @GetMapping("/")
+    public String home() {
+        return "Hello bro, Docker + Maven + Jenkins deployment is working on EC2!";
     }
 }
